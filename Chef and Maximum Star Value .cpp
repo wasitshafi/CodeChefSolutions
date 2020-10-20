83684dc55060992a56708d1a0c6bdd6f99749e8e
WasitShafi <wasitshafi700@gmail.com>
2020-10-20 12:06:44 +0530

    added new files

--- Chef and Maximum Star Value .cpp
+++ Chef and Maximum Star Value .cpp
@@ -1,42 +1,27 @@
+// Q : https://www.codechef.com/OCT19B/problems/MSV/
+#include<iostream>
 
-       if(arr[i] == 1)
-                count = i;
-            else
-            { if(arr[i] == 1)
-                count = i;
-            else
-            {  for(int i = 1 ; i < size ; i++)
+using namespace std;
+
+int main()
+{
+    int noOfTestCases, size, count, maxCount;
+    
+    cin >> noOfTestCases;
+    
+    while(noOfTestCases--)
+    {
+        
+        cin >> size;
+        int *arr = new int[size];
+
+        for(int i = 0 ; i < size ; i++) cin >> arr[i];
+
+        maxCount = 0;
+        for(int i = 1 ; i < size ; i++)
         {
             count = 0;;
             if(arr[i] == 1)
-                count = i;
-            else
-            {
-                for(int j = 0 ; j < i ; j++)
-                    if(!(arr[j] % arr[i]))
-                        count++;
-            }            if(arr[i] == 1)
-                count = i;
-            else
-            {
-                for(int j = 0 ; j < i ; j++)
-                    if(!(arr[j] % arr[i]))
-                        count++;
-            }            if(arr[i] == 1)
-                count = i;
-            else
-            {
-                for(int j = 0 ; j < i ; j++)
-                    if(!(arr[j] % arr[i]))
-                        count++;
-            }            if(arr[i] == 1)
-                count = i;
-            else
-            {
-                for(int j = 0 ; j < i ; j++)
-                    if(!(arr[j] % arr[i]))
-                        count++;
-            }            if(arr[i] == 1)
                 count = i;
             else
             {
